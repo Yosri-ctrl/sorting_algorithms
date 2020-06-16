6b8f7c7667b1b55002b4168b4dfafbbaf281d94f
Yosri-ctrl <yosribouabid@gmail.com>
2020-06-16 11:18:16 +0100

    header

--- sort.h
+++ sort.h
@@ -1,5 +1,7 @@
 #ifndef _SORT_H_
 #define _SORT_H_
+#include <stdio.h>
+#include <stdlib.h>
 
 /*structures*/
 
@@ -21,5 +23,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif
