66607924feb83b9659fd902b6901f2fb67013b99
Yosri-ctrl <yosribouabid@gmail.com>
2020-06-16 11:20:45 +0100

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,27 +6,17 @@
 void bubble_sort(int *a, size_t s)
 {
     size_t i, j, aux;
-
-    /*printf("before\n");
-    print_array(a, s);;*/
-
-        for (i = 0; i < s-1; i++)
+    for (i = 0; i < s-1; i++)
+    {
+        for (j = 0; j < s-1; j++)
         {
-            for (j = 0; j < s-1; j++)
+            if (a[j] > a[j + 1])
             {
-                if (a[j] > a[j + 1])
-                {
-
-                    aux = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = aux;
-                    print_array(a, s);
-                }
-                /*printf("between\n");
-                print_array(a, s);*/
+                aux = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = aux;
+                print_array(a, s);
             }
         }
-
-    /*printf("\n\nafter\n");
-    print_array(a, s);*/
+    }
 }
