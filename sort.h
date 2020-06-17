8d3e306fec219f492521fbc06dcf1587ef8d9137
Yosri-ctrl <yosribouabid@gmail.com>
2020-06-17 13:08:50 +0100

    betty

--- sort.h
+++ sort.h
@@ -14,9 +14,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /*prototypes*/
@@ -24,5 +24,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
