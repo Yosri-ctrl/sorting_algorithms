f3b72664cdbabcc1086d1f6a35d08243c50acfd6
Yosri-ctrl <yosribouabid@gmail.com>
2020-06-17 13:07:54 +0100

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,22 +1,24 @@
 #include "sort.h"
 /**
-*
-*
+*bubble_sort - bubble sort
+*@a: the array to treat
+*@s: the size of the array
 **/
 void bubble_sort(int *a, size_t s)
 {
-    size_t i, j, aux;
-    for (i = 0; i < s-1; i++)
-    {
-        for (j = 0; j < s-1; j++)
-        {
-            if (a[j] > a[j + 1])
-            {
-                aux = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = aux;
-                print_array(a, s);
-            }
-        }
-    }
+	size_t i, j, aux;
+
+	for (i = 0; i < s - 1; i++)
+	{
+		for (j = 0; j < s - 1; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				aux = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = aux;
+				print_array(a, s);
+			}
+		}
+	}
 }
